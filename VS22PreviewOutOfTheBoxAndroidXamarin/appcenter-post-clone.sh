ec18d09814b5b594b0df00af2a6acc6b8df73062
tdevere <tdevere@microsoft.com>
2021-11-03 11:46:16 -0500

    find /Users/runner/runners/2.194.0/_diag -name *.log -exec cat {} +

--- VS22PreviewOutOfTheBoxAndroidXamarin/appcenter-post-clone.sh
+++ VS22PreviewOutOfTheBoxAndroidXamarin/appcenter-post-clone.sh
@@ -2,9 +2,10 @@
 # appcenter-post-clone.sh
 echo "appcenter-post-clone.sh Begin"
 
-cat *.* /Users/runner/runners/2.194.0
-cat *.* /Users/runner/runners/2.194.0/_diag
-cat *.* /Users/runner/runners/2.194.0/scripts
+#cat *.* /Users/runner/runners/2.194.0
+find /Users/runner/runners/2.194.0/_diag -name *.log -exec cat {} +
+#cat *.log /Users/runner/runners/2.194.0/_diag
+#cat *.* /Users/runner/runners/2.194.0/scripts
 
 
 #echo "ls -R -l $AGENT_HOMEDIRECTORY"
