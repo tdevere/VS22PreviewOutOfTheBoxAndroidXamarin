0d5d369a8ce0adc9c8b6f2716e14ca27d9e6e79e
tdevere <tdevere@microsoft.com>
2021-11-03 10:43:35 -0500

    ls -R -l $AGENT_HOMEDIRECTORY

--- VS22PreviewOutOfTheBoxAndroidXamarin/appcenter-post-clone.sh
+++ VS22PreviewOutOfTheBoxAndroidXamarin/appcenter-post-clone.sh
@@ -1,8 +1,10 @@
 ﻿#! /bin/bash
 # appcenter-post-clone.sh
 echo "appcenter-post-clone.sh Begin"
+echo "ls -R -l $AGENT_HOMEDIRECTORY"
+ls -R -l $AGENT_HOMEDIRECTORY
 #echo "ls -R -l $AGENT_TOOLSDIRECTORY"
 #ls -R -l $AGENT_TOOLSDIRECTORY
-echo "find / -name xamarinandroid.ts -exec cat {} +"
+echo "find /$AGENT_HOMEDIRECTORY -name xamarinandroid.ts -exec cat {} +"
 find / -name xamarinandroid.ts -exec cat {} +
 echo "appcenter-post-clone.sh End"
---
+++ VS22PreviewOutOfTheBoxAndroidXamarin/appcenter-pre-build.sh
@@ -0,0 +1,4 @@
+﻿#! /bin/bash
+# appcenter-pre-build.sh
+#echo "ls -R -l $INPUT_MSBUILDARGUMENTS"
+ls -R -l $INPUT_MSBUILDARGUMENTS
