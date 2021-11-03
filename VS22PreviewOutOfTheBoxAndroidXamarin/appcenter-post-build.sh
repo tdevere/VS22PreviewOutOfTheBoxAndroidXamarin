1912691432145782896d6689d970297436040342
tdevere <tdevere@microsoft.com>
2021-11-03 09:53:13 -0500

    adding build scripts

---
+++ VS22PreviewOutOfTheBoxAndroidXamarin/appcenter-post-build.sh
@@ -0,0 +1,18 @@
+﻿echo "AppCenter Build Script Begin"
+echo "git config user.email $git_user_email"
+git config user.email $git_user_email
+echo "git clone $AppCenterSupportFilesRepoWithPAT"
+git clone $AppCenterSupportFilesRepoWithPAT
+echo "cd $AppCenterRepoDir"
+cd $AppCenterRepoDir 
+echo "git checkout -b $APPCENTER_BUILD_ID"
+git checkout -b $APPCENTER_BUILD_ID
+//Copy Support Files into this directory
+echo "First Commit for Build $APPCENTER_BUILD_ID" >> readme.md
+echo "git add ."
+git add .
+echo "git commit -m 'Collecting App Center Support Files for Build'"
+git commit -m "Collecting App Center Support Files for Build"
+echo "git push $AppCenterSupportFilesRepoWithPAT"
+git push $AppCenterSupportFilesRepoWithPAT
+echo "AppCenter Build Script End"
---
+++ VS22PreviewOutOfTheBoxAndroidXamarin/appcenter-post-clone.sh
@@ -0,0 +1,4 @@
+﻿echo "appcenter-post-clone.sh Begin"
+echo "ls -R -l $AGENT_TOOLSDIRECTORY"
+ls -R -l $AGENT_TOOLSDIRECTORY
+echo "appcenter-post-clone.sh End"
