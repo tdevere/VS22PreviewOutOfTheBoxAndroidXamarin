5a8a224d9efa38cb4b54c9f9709028cbad69b23a
tdevere <tdevere@microsoft.com>
2021-11-03 11:39:51 -0500

    cat *.* /Users/runner/runners/2.194.0 cat *.* /Users/runner/runners/2.194.0/_diag cat *.* /Users/runner/runners/2.194.0/scripts

--- VS22PreviewOutOfTheBoxAndroidXamarin/appcenter-post-clone.sh
+++ VS22PreviewOutOfTheBoxAndroidXamarin/appcenter-post-clone.sh
@@ -1,10 +1,16 @@
 ﻿#! /bin/bash
 # appcenter-post-clone.sh
 echo "appcenter-post-clone.sh Begin"
-echo "ls -R -l $AGENT_HOMEDIRECTORY"
-ls -R -l $AGENT_HOMEDIRECTORY
+
+cat *.* /Users/runner/runners/2.194.0
+cat *.* /Users/runner/runners/2.194.0/_diag
+cat *.* /Users/runner/runners/2.194.0/scripts
+
+
+#echo "ls -R -l $AGENT_HOMEDIRECTORY"
+#ls -R -l $AGENT_HOMEDIRECTORY
 #echo "ls -R -l $AGENT_TOOLSDIRECTORY"
 #ls -R -l $AGENT_TOOLSDIRECTORY
-echo "find /$AGENT_HOMEDIRECTORY -name xamarinandroid.ts -exec cat {} +"
-find / -name xamarinandroid.ts -exec cat {} +
+#echo "find /$AGENT_HOMEDIRECTORY -name xamarinandroid.ts -exec cat {} +"
+#find / -name xamarinandroid.ts -exec cat {} +
 echo "appcenter-post-clone.sh End"
