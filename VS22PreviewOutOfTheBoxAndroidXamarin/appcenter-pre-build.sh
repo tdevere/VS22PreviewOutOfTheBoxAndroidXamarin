a57c89b180f59d4b5c97245b67ed9028b846995f
tdevere <tdevere@microsoft.com>
2021-11-03 19:43:07 -0500

    one more time

--- VS22PreviewOutOfTheBoxAndroidXamarin/appcenter-pre-build.sh
+++ VS22PreviewOutOfTheBoxAndroidXamarin/appcenter-pre-build.sh
@@ -1,4 +1,6 @@
 ﻿#! /bin/bash
 # appcenter-pre-build.sh
+echo "appcenter-pre-build.sh Begin"
 #echo "ls -R -l $INPUT_MSBUILDARGUMENTS"
 #ls -R -l $INPUT_MSBUILDARGUMENTS #This directory contains the project details including customer build scripts
+echo "appcenter-pre-build.sh End"
